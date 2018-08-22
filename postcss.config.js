64b93397064e5a9608d8932e2311fdc2591fb28a
Nicholas Scheurich <nick@scheurich.me>
2018-08-21 22:17:29 -0500

    Update PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssImport = require('postcss-import');
+const tailwindcss = require('tailwindcss');
+const cssNano = require('cssnano');
+
 module.exports = {
   plugins: [
-    require('postcss-import')(),
-    require('tailwindcss')('./tailwind.js')
+    postcssPresetEnv({ stage: 0 }),
+    postcssImport(),
+    tailwindcss('./tailwind.js'),
+    cssNano()
   ]
 };
